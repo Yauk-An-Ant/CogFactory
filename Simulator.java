797bf4bad28e28658b4e25d135a8f602b45be814
Yauk-An-Ant <95669517+Yauk-An-Ant@users.noreply.github.com>
2023-11-30 11:56:44 -0500

    made the thing work

--- Simulator.java
+++ Simulator.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Simulator {
     
     ArrayList<Worker> workers;
-    static Queue<Integer> cogOrders;
+    Queue<Integer> cogOrders;
     ArrayList<Integer> cogList;
     private boolean log = true;
 
@@ -22,15 +22,17 @@ public class Simulator {
 
     public void run() {
 
+        
         PriorityQueue<Integer> numbers = new PriorityQueue<Integer>();
         while(!cogOrders.isEmpty()) {
             numbers.add(cogOrders.poll());
         }
-
+        /* 
         PriorityQueue<Worker> priorityWorkers = new PriorityQueue<Worker>();
         for(Worker w : workers) {
             priorityWorkers.add(w);
         }
+        */
         
 
         /*
@@ -40,38 +42,47 @@ public class Simulator {
         }
         */
 
-        while(!priorityWorkers.isEmpty()) {
-            if(!numbers.isEmpty()) {
-                if(!priorityWorkers.peek().isBusy())
-                    priorityWorkers.poll().assignOrder(numbers.poll());
-            }
+        /* 
+        while(!priorityWorkers.isEmpty() && !numbers.isEmpty()) {
+            if(!priorityWorkers.peek().isBusy())
+                priorityWorkers.poll().assignOrder(numbers.poll());
         }
+        */
+
+        
         
         int hours = 0;
         
-        Queue<Worker> q = new LinkedList<>();
+        PriorityQueue<Worker> priorityWorkers = new PriorityQueue<Worker>();
+        for(Worker w : workers) {
+            priorityWorkers.add(w);
+        }
+
+        while(!priorityWorkers.isEmpty()) {
+            priorityWorkers.poll().assignOrder(numbers.poll());
+        }
         
         while (! (cogOrders.isEmpty() && allDone()) ) {
-        
             hours++;
-            for (Worker w: workers) {
+
+            for (Worker w : workers) {
                 w.workOneHour();
                 if (!w.isBusy())
-                    q.add(w);
+                    priorityWorkers.add(w);
             }
         
-            while(!q.isEmpty()){
-                Worker w = q.poll();
-                if (!cogOrders.isEmpty())
-                    w.assignOrder(cogOrders.poll());
+            while(!priorityWorkers.isEmpty()){
+                Worker w = priorityWorkers.poll();
+                if (!numbers.isEmpty())
+                    w.assignOrder(numbers.poll());
             }
             
         }
 
         if(log) {
             //print relevant details
+            System.out.println("Hours: " + hours);
             for(int i = 0; i < workers.size(); i++) {
-                System.out.println("Hours: " + hours);
                 System.out.println("Name: " + workers.get(i).getName());
                 System.out.println("CPH: " + workers.get(i).getCph());
                 System.out.println("Total Cogs Produced: " + workers.get(i).getTotalCogsProduced());
@@ -95,10 +106,10 @@ public class Simulator {
         workers.add(new Worker("A", 30));
         workers.add(new Worker("B", 25));
         ArrayList<Integer> cogs = new ArrayList<Integer>();
-        cogs.add(100);
-        cogs.add(60);
         cogs.add(800);
-        cogs.add(25);
+        cogs.add(60);
+        cogs.add(80);
+        cogs.add(5);
         cogs.add(45);
         Simulator fs = new Simulator(workers, cogs);
         fs.run();
